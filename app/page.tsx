211866876fcd836258e27220eafe9dbd2586b3a7
rizky <96133536+hellookii@users.noreply.github.com>
2025-03-07 20:25:57 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ButtonComponent from "@/components/ButtonComponent";
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen px-4">
-      <h1 className="text-[#672cbc] font-bold text-3xl">Quran App</h1>
+      <h1 className="text-[#672cbc] font-bold text-3xl">Quran App Muhammad Rizky AbdilIah</h1>
       <section className="py-4 space-y-1 text-center text-black/60">
         <p>Learn Quran and Recite</p>
         <p>once everyday</p>
